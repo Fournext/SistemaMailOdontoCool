310c923d3a7533f7553d1696483de9faf32afd7a
Fournext <sebastianzc04@gmail.com>
2026-05-29 08:07:52 -0400

    feat: añadir decorador para traccion de errores bd a spring

--- src/main/java/smail/sistema_mail_OdontoCool/repositories/PacienteRepository.java
+++ src/main/java/smail/sistema_mail_OdontoCool/repositories/PacienteRepository.java
@@ -1,7 +1,10 @@
 package smail.sistema_mail_OdontoCool.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
 import smail.sistema_mail_OdontoCool.entities.Paciente;
 
+@Repository
 public interface PacienteRepository extends JpaRepository<Paciente, String> {
 }
